148750e5ed782d33ca1354ef9c6cafb7d9a367bc
carladantas <carla.s.dantas@hotmail.com>
2017-01-15 17:26:14 +0000

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,47 @@
+function askQuestions () {
+
+	var firstName = prompt('What is your first name?'),
+		lastName = prompt('What is your last name?'), 
+		fullName = firstName + ' ' + lastName;
+
+	console.log(fullName);
+
+	var age = prompt('How old are you?');
+	age = parseInt(age);
+
+	if (age >= 18) {
+
+		console.log('User is an adult');
+
+	}else{
+
+		console.log('User is a child');
+
+	}	
+
+	/*
+		If the user's first name is 'General' and 
+		their last name is NOT 'Assembly', then greet the general!
+		(Hint: Use console.log) 
+	*/
+
+	if (firstName === 'General' && lastName !== 'Assembly') {
+
+	    	console.log('Greetings General!'); 
+		}
+}
+
+// When the page has loaded 
+
+$(function () {
+
+	// When the user clicks an h3 element
+	$('h3').on('click', function () {
+
+	// Toggle the next element
+	$(this).next().slideToggle(9000);
+
+	});
+
+});
+
